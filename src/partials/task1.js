4aa3f30953abda69bce00c8205428e7b3e19318a
Yana Sokurenko <yanamoss@gmail.com>
2021-07-12 13:58:51 +0300

    task-11

---
+++ src/partials/task1.js
@@ -0,0 +1,25 @@
+'use strict'
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+console.log(body);
+buttonStart.addEventListener('click', startChange);
+buttonStop.addEventListener('click', stopChange);
+let timeChangeColor = null;
+
+function startChange(event) {
+  function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+  function startChangeColor() {
+      body.style.backgroundColor = getRandomHexColor();
+      buttonStart.setAttribute("disabled", "disabled");
+   };
+   timeChangeColor = setInterval(startChangeColor, 1000);
+};
+
+function stopChange(event) {
+   buttonStart.removeAttribute("disabled");
+   clearInterval(timeChangeColor);
+
+};
