9c155518bf1ff89b707c0669cac1cdb92cef319c
Yana Sokurenko <yanamoss@gmail.com>
2021-08-04 14:30:17 +0300

    test

--- src/partials/1task.js
+++ src/partials/1task.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
